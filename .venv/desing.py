465f6b2efb11179a8f4f975634dc24abe8e74c9e
PabloHoracioVelazquezTelle <pablotz1504@gmail.com>
2024-10-30 00:07:35 -0600

    3rd commit

---
+++ .venv/desing.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from tkinter import ttk
+
+class Desing:
+    def setup_ui(self, root):
+        # Tabla para mostrar los registros
+        estilo = ttk.Style()
+        estilo.theme_use("default")
+        estilo.configure("Custom.Treeview", background="black", foreground="green", fieldbackground="black",
+                         font=("Arial", 15))
+        estilo.map("Custom.Treeview", background=[("selected", "black")], foreground=[("selected", "green")])
+
+        self.tree = ttk.Treeview(root, columns=("ID", "Nombre", "Apellido", "Ciudad", "Calle", "Promedio"),
+                                 show="headings", style="Custom.Treeview")
+        self.tree.heading("ID", text="ID")
+        self.tree.heading("Nombre", text="Nombre")
+        self.tree.heading("Apellido", text="Apellido")
+        self.tree.heading("Ciudad", text="Ciudad")
+        self.tree.heading("Calle", text="Calle")
+        self.tree.heading("Promedio", text="Promedio")
+        self.tree.column("ID", width=50, anchor='center', stretch=False)
+        self.tree.column("Nombre", width=125, anchor='w', stretch=False)
+        self.tree.column("Apellido", width=155, anchor='w', stretch=False)
+        self.tree.column("Ciudad", width=160, anchor='w', stretch=False)
+        self.tree.column("Calle", width=200, anchor='w', stretch=False)
+        self.tree.column("Promedio", width=70, anchor='w', stretch=False)
+        self.tree.pack(pady=10, fill="both", expand=True)
